fde8ab0f1f41a5f80a845e95c19b8c1960b3d4ec
jaco8865 <jacoswart8865@gmail.com>
2021-07-05 06:57:07 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: 'https://v8ympiu595.execute-api.eu-west-1.amazonaws.com/Predict_Lambda',
+                url: 'https://xt1dp2iw6d.execute-api.eu-west-1.amazonaws.com/Predict_Lambda',
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
